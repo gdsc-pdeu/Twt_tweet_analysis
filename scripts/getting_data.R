44d81c9f276dab6fbb9523dc0a0e79bd5a565ab2
ThenoobMario <soni.nachiket@yahoo.in>
2020-06-02 12:05:39 +0530

    added commands to exlude replies

--- scripts/getting_data.R
+++ scripts/getting_data.R
@@ -4,7 +4,7 @@ t_user <- getUser("Enter Username here")
 
 # The following function takes n = 20 as default and can take upto 3200 tweets
 
-u_tweets <- userTimeline("Enter Username here")
+u_tweets <- userTimeline("Enter Username here", excludeReplies = TRUE)
 
 # Converting the list into a dataframe for easier analysis
 
