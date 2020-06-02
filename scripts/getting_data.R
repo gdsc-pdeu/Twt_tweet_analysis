13f492a16c367487a54cbb4b90063dee1add8e1b
ThenoobMario <soni.nachiket@yahoo.in>
2020-06-02 11:24:37 +0530

    Getting the tweets from the internet

---
+++ scripts/getting_data.R
@@ -0,0 +1,11 @@
+## GEtting the user information
+
+t_user <- getUser("Enter Username here")
+
+# The following function takes n = 20 as default and can take upto 3200 tweets
+
+u_tweets <- userTimeline("Enter Username here")
+
+# Converting the list into a dataframe for easier analysis
+
+u_tweetsdf <- twListToDF(u_tweets)
